26d1248367ad3e2a25831f16d98caaa2dd53586b
Stefano Germano <stefanogermano0@gmail.com>
2020-12-12 23:41:39 +0100

    Fix packaging/installation

--- setup.py
+++ setup.py
@@ -9,29 +9,17 @@ setup(
     packages=find_packages(),
     scripts=['embasp_server_executor/ese_main.py'],
 
-    install_requires=['embasp', 'tornado'],
+    install_requires=['embasp', 'tornado', 'antlr4-python3-runtime'],
     package_data={
         '': ['*.txt', '*.md'],
     },
 
-    test_suite="embasp_server_executor.tests",
-    # test_suite A string naming a unittest.TestCase subclass (or a package or module containing one or more of them,
-    # or a method of such a subclass), or naming a function that can be called with no arguments and returns a
-    # unittest.TestSuite. If the named suite is a module, and the module has an additional_tests() function,
-    # it is called and the results are added to the test to be run. If the named suite is a package, any submodules
-    # and subpackages are recursively added to the overall test suite. Specifying this argument enables use of the
-    # test command to run the specified test suite, e.g. via setup.py test. See the section on the test command below
-    # for more details. tests_require If your project’s test need one or more additional packages besides those
-    # needed to install it, you can use this option to specify them. It should be a string or list of strings
-    # specifying what other distributions need to be present for the package’s test to run. When you run the test
-    # command, setuptools will attempt to obtain these (even going so far as to download them using EasyInstall).
-    # Note that these required projects will not be installed on the system where the test are run,
-    # but only downloaded to the project’s setup directory if they’re not already installed locally.
+    test_suite="tests",
 
     # metadata to display on PyPI
     author="Stefano Germano",
     author_email="loide@mat.unical.it",
-    description="Python web app to execute logic programs with different solvers, using the EmbASP framework",
+    description="Python web-app to execute logic programs with different solvers, using the EmbASP framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
@@ -42,13 +30,12 @@ setup(
         "Documentation": "",
         "Source Code": "https://github.com/DeMaCS-UNICAL/PythonESE",
     },
+    download_url="https://github.com/DeMaCS-UNICAL/PythonESE/releases",
 
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-
-    # could also include download_url, etc.
 
 )
