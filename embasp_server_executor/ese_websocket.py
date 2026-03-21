81968e49c2694aa868547a5be4ea44b359b1128c
Stefano Germano <2128484+stefanogermano@users.noreply.github.com>
2026-03-21 23:47:49 +0100

    Improved callback handling and other minor fixes

--- embasp_server_executor/ese_websocket.py
+++ embasp_server_executor/ese_websocket.py
@@ -1,13 +1,13 @@
-from asyncio import set_event_loop, new_event_loop
+from asyncio import new_event_loop, set_event_loop
 from datetime import datetime
 from urllib.parse import urlparse
 
+from embasp.base.callback import Callback
+from tornado.ioloop import IOLoop
 from tornado.websocket import WebSocketHandler
 
-from embasp.base.callback import Callback
-
 from .ese_config import config as ec
-from .ese_utils import process_program_and_options, get_output_data
+from .ese_utils import get_output_data, process_program_and_options
 
 
 class ESEWebSocket(WebSocketHandler, Callback):
@@ -15,13 +15,15 @@ class ESEWebSocket(WebSocketHandler, Callback):
     This is the WebSocket handler class. It has been extended to call EmbASP when a message is received.
     """
 
-    # Uncomment this if you want to test out the script on localhost
+    io_loop: IOLoop = None
+
     def check_origin(self, origin):
         parsed_origin = urlparse(origin)
-        # # parsed_origin.netloc.lower() gives localhost:3333
         return parsed_origin.hostname in ec.cors_origins
 
     def open(self):
+        # Capture the *current* IOLoop instance - this is the one running the WebSocket
+        self.io_loop = IOLoop.current()
         print("\n\n", datetime.now(), "\nWebSocket opened")
 
     def on_close(self):
@@ -35,17 +37,30 @@ class ESEWebSocket(WebSocketHandler, Callback):
         o_output, o_errors = "", ""
 
         if o.get_output() is not None:
-            if len(o.get_output()) > ec.max_chars_output:  # Check if the solver output is too long
+            # Check if the solver output is too long
+            if len(o.get_output()) > ec.max_chars_output:
                 o_output = (o.get_output()[:ec.max_chars_output] + "\n[...]\nDo you need more? Let us know")
             else:
                 o_output = o.get_output()
 
         if o.get_errors() is not None:
-            if len(o.get_output()) > ec.max_chars_output:  # Check if the solver output is too long
+            # Check if the solver error is too long
+            if len(o.get_errors()) > ec.max_chars_output:
                 o_errors = (o.get_errors()[:ec.max_chars_output] + "\n[...]\nDo you need more? Let us know")
             else:
                 o_errors = o.get_errors()
 
-        set_event_loop(new_event_loop())
-        self.write_message(get_output_data(model=o_output, error=o_errors))
-        print("Sent data", get_output_data(model=o_output, error=o_errors))
+        # Use the stored loop
+        if self.io_loop is None:
+            print("Callback called before WebSocket opened!", file=sys.stderr)
+            return
+
+        self.io_loop.add_callback(self._write_result, o_output, o_errors)
+
+    def _write_result(self, model: str, error: str):
+        try:
+            data = get_output_data(model=model, error=error)
+            self.write_message(data)
+            print("Sent data", data)
+        except Exception as e:
+            print(f"Failed to write message: {e}", file=sys.stderr)
